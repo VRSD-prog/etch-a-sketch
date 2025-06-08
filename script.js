d01e311384677cc8a2b828a32181c0d46639236a
VRSD-prog <cyreeka2016@gmail.com>
2025-06-08 10:57:54 +0800

    Fixed click-and-drag drawing bug Added logic for color and eraser features

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
+//Main Container
+const mainContainer = document.querySelector("#main-container")
+
+
+//Initial Grid
+for (i=0; i<16; i++) {
+        const columnDivInit = document.createElement("div")
+        columnDivInit.classList.add("column-div")
+        columnDivInit.style.width = `${650/16}px`
+        columnDivInit.style.height = '650px'
+        mainContainer.appendChild(columnDivInit)
+    } 
+
+const columnsInit = document.querySelectorAll(".column-div")
+
+for (const divInit of columnsInit ) {
+    for (i=0; i<16; i++) {
+        const singularDivInit = document.createElement("div")
+        singularDivInit.classList.add("singular-div")
+        singularDivInit.style.height = `${650/16}px` 
+        divInit.appendChild(singularDivInit)
+    }
+
+const boxes = document.querySelectorAll(".singular-div")
+for (const box of boxes) {
+    box.addEventListener("dragstart", (e) => e.preventDefault())
+    box.addEventListener("mousemove", () => {
+    if (event.buttons === 1){
+        box.style.backgroundColor = `${color}`
+    }
+    })
+}
+}
+
 //Box Count Input
 const input = document.querySelector("#size-input")
 let userInput
@@ -16,6 +50,7 @@ input.addEventListener("keydown", (e) => {
 //Color Picker
 const colorInput = document.querySelector("#color-input")
 let color = "black"
+const colorHistory = ["black"]
 
 if (color === null) {
     color = "black"
@@ -23,15 +58,28 @@ if (color === null) {
 
 colorInput.addEventListener("keydown", (e) => {
     if (e.key === "Enter"){
+        e.preventDefault()
         color = `${colorInput.value}`
+        colorHistory.push(`${color}`)
         console.log(color)
+        console.log(colorHistory)
     }
 })
 
-
-//Main Container
-const mainContainer = document.querySelector("#main-container")
-
+//Eraser
+const eraser = document.querySelector(".eraser-button")
+eraser.addEventListener("click", (e) => {
+    e.preventDefault()
+    if (eraser.textContent === "Disabled") {
+        eraser.textContent = "Enabled"
+        color = "white"
+        console.log(colorHistory)
+    } else if (eraser.textContent === "Enabled") {
+        eraser.textContent = "Disabled"
+        color = `${colorHistory[colorHistory.length - 1]}`
+        console.log(colorHistory)
+    }
+})
 
 //Grid Creation Logic
 function createDivs(userInput) {
@@ -59,24 +107,12 @@ function createDivs(userInput) {
 
     //Drawing Logic
     const boxes = document.querySelectorAll(".singular-div")
-    let isDrawing = false
     for (const box of boxes) {
-        box.addEventListener("mousedown", () => {
-        isDrawing = true
-    })
-    }
-
-    for (const box of boxes) {
-        box.addEventListener("mouseup", () => {
-        isDrawing = false   
-    })
-    }
-
-    for (const box of boxes) {
-        box.addEventListener("mouseenter", () => {
-            if (isDrawing === true) {
-                box.style.backgroundColor = `${color}`
-            }
+        box.addEventListener("dragstart", (e) => e.preventDefault())
+        box.addEventListener("mousemove", () => {
+        if (event.buttons === 1){
+            box.style.backgroundColor = `${color}`
+        }
         })
     }
 }
@@ -84,5 +120,17 @@ function createDivs(userInput) {
 
 /*
 Where I left off:
--
+-Trying to fix eraser button using ChatGPT
+
+Features to Add:
+-Eraser
+-Undo Button
+
+To Do:
+-Apply the drawing event listeners to the entire document
+
+Bugs:
+-Eraser button is not reverting to previous color after Enabled -> Disabled
+-Drawing over canvas even when the mouse is not held down
+
 */
